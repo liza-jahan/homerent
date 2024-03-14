d37c2aa69e46b0eef8160659410805d20482cb3d
Eakteker jahan <eaktekerjahan@gmail.com>
2024-03-15 01:02:18 +0600

    Create Registration

---
+++ src/main/java/com/example/homerent/HomerentApplication.java
@@ -0,0 +1,13 @@
+package com.example.homerent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomerentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomerentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homerent/HomerentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homerent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomerentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
