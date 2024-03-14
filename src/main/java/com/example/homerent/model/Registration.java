5bdd52f2c5afeca16e3e48e9b9625cb600dfc3fc
Eakteker jahan <eaktekerjahan@gmail.com>
2024-03-15 01:01:22 +0600

    Create Registration

---
+++ src/main/java/com/example/homerent/model/Registration.java
@@ -0,0 +1,29 @@
+package com.example.homerent.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Registration {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    private  String id;
+    private String name;
+    private String phoneNumber;
+    private String passWord;
+    private String confirmPassword;
+
+}
